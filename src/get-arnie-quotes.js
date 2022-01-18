d47c5cc894d623b01ebca620adb78df77b185bc9
Joshua Tu <tjh8523@gmail.com>
2022-01-19 07:14:17 +1100

    feat: complete the test

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,18 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const httpGetPromises = urls.map(url => httpGet(url));
+
+  const responses = await Promise.all(httpGetPromises);
+
+	const results = responses.map(response => {
+    const responseBodyObject = JSON.parse(response.body)
+    if (response.status === 200) return { 'Arnie Quote': responseBodyObject.message };
+    if (response.status === 500) return { 'FAILURE': responseBodyObject.message };
+  })
+  
+  return results;
+
 };
 
 module.exports = {
